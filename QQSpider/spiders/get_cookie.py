f540e5b100e4297f1201dd7291ad42f617d41a0e
Samray Leung <samrayleung@gmail.com>
2017-06-25 13:01:17 +0800

    stop maintaining

--- QQSpider/spiders/get_cookie.py
+++ QQSpider/spiders/get_cookie.py
@@ -13,12 +13,13 @@ class GetCookie(object):
         """ 根据QQ号和密码获取cookie """
         failure = 0
         while failure < 2:
+            logging.debug("start get gtk")
             try:
                 browser = webdriver.PhantomJS(
                     executable_path="/usr/local/bin/phantomjs")
                 wait = ui.WebDriverWait(browser, 10)
                 browser.get(
-                    'http://qzone.qq.com/?s_url=http://user.qzone.qq.com/869810754/')
+                    'http://qzone.qq.com/?s_url=http://user.qzone.qq.com/1342512151/')
                 browser.switch_to_frame('login_frame')
                 wait.until(
                     lambda browser: browser.find_element_by_id('switcher_plogin'))
@@ -30,15 +31,19 @@ class GetCookie(object):
                 p = browser.find_element_by_id('p')
                 p.send_keys('%s' % (password))
                 wait.until(
-                    lambda browser: browser.find_element_by_xpath('//*[@id="login_button"]'))
-                login = browser.find_element_by_xpath(
-                    '//*[@id="login_button"]')
+                    # lambda browser: browser.find_element_by_xpath('//*[@id="login_button"]'))
+                    lambda browser: browser.find_element_by_xpath("id('login_button')"))
+                # login = browser.find_element_by_xpath(
+                #     '//*[@id="login_button"]')
+                login = browser.find_element_by_xpath("id('login_button)")
+                logging.debug("try logging")
                 time.sleep(2)
                 login.click()
                 time.sleep(1)
                 try:
                     browser.switch_to_frame('vcode')
                     print('Failed!----------------reason:该QQ首次登录Web空间，需要输入验证码！')
+                    logging.error("vcode error")
                     break
                 except Exception:
                     pass
@@ -47,19 +52,22 @@ class GetCookie(object):
                     time.sleep(2)
                     d = err.text
                     print(account, d)
-                    if u'您输入的帐号或密码不正确' in d:
+                    logging.error("Error occur")
+                    if '您输入的帐号或密码不正确' in d:
                         print('Failed!----------------reason:账号或者密码错误！')
                         logging.error("账号或者密码错误")
                         break
-                    if u'网络繁忙' in d:
+                    if '网络繁忙' in d:
                         time.sleep(2)
                 except Exception as e:
+                    logging.error("erorr:{}".format(e))
                     cookie = {}
                     for ck in browser.get_cookies():
                         cookie[ck['name']] = ck['value']
                         browser.quit()
                         print("Get the cookie of QQ:%s successfully!(共%d个键值对" % (
                             account, len(cookie)))
+                    logging.debug("cookie: {}", dict(cookie))
                     return cookie
             except Exception:
                 failure = failure + 1
